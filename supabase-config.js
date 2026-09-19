daf7d326ede304f3456613e95a16224ededad542
saeedanowarwb-beep <saeedanowarwb@gmail.com>
2026-09-19 19:30:49 +0530

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,15 +1,15 @@
-// =========================================================
+// =====================================================
 // Dr. Reshma Khatun Academic Portal
 // Supabase Configuration
-// =========================================================
+// =====================================================
 
-const SUPABASE_URL = "https://uupdkljuuavsxxrefmzr.supabase.co";
+const SUPABASE_URL = "https://uupdkljuavsxkrefmzr.supabase.co";
 
 const SUPABASE_PUBLISHABLE_KEY =
-  "sb_publishable_GC-wNHVFIGCxNujk7t1XQA_jnXdVqcb";
+  "sb_publishable_GC-wNHVFIGCxNujgk7t1XQA_jnxXdVqcb";
 
 // Create Supabase client
-const supabaseClient = window.supabase.createClient(
+window.supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_PUBLISHABLE_KEY
 );
