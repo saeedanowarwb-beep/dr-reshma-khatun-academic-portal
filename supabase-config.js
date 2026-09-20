2001693f61b2633aff7ec88417860e0986bfac61
saeedanowarwb-beep <saeedanowarwb@gmail.com>
2026-09-20 17:19:54 +0530

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://uupdkljuuavsxxrefmzr.supabase.co";
 const SUPABASE_PUBLISHABLE_KEY =
-  "sb_publishable_GC-wNHVFIGCxNujgk7t1XQA_jnxXdVqcb";
+  "sb_publishable_GC-wNHVFIGCxNujgk7t1XQA_jnXdVqcb";
 
 const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
